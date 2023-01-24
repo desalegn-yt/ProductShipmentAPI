ab97e818ccf3ca1cc7b1d8e4ad9002e1fc19a154
Rani Adam <rani.adam@gmail.com>
2023-01-24 22:38:46 +1100

    infra fixes

--- SmartNestAPI/infra/alb.sg.tf
+++ SmartNestAPI/infra/alb.sg.tf
@@ -6,16 +6,4 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
