5dd7f7a95b1a868552aa2aad00f299b829e588b9
Desalegn Yismaw <DesalegnYi@ethiopianairlines.com>
2023-10-12 08:25:29 +0300

    first commit

--- SmartNestAPI.Infrastructure/DataAccess/PostgreSqlContext.cs
+++ SmartNestAPI.Infrastructure/DataAccess/PostgreSqlContext.cs
@@ -37,7 +37,7 @@ namespace SmartNestAPI.Domain.Entities.Database
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=smartnest1.czigbhm0yn5q.ap-southeast-2.rds.amazonaws.com;Username=postgres;Password=wMSW#35a9K1y;Database=smartnest");
+                optionsBuilder.UseNpgsql("Your connection string");
             }
         }
 
